44c47187bbd8c5f8e0df631398cae656646ddcdd
eysp <yspvps@gmail.com>
2025-11-05 14:01:26 +0800

    去除官方提示，去除升级

--- app/react/components/InformationPanel.tsx
+++ app/react/components/InformationPanel.tsx
@@ -21,26 +21,7 @@ export function InformationPanel({
   return (
     <Widget className="border-none">
       <WidgetBody className={bodyClassName}>
-        <div style={wrapperStyle}>
-          {title && (
-            <div className="form-section-title">
-              <span>{title}</span>
-              {!!onDismiss && (
-                <span className="small" style={{ float: 'right' }}>
-                  <Button
-                    color="link"
-                    icon={X}
-                    onClick={() => onDismiss()}
-                    data-cy="dismiss-information-panel-button"
-                  >
-                    dismiss
-                  </Button>
-                </span>
-              )}
-            </div>
-          )}
-          <div>{children}</div>
-        </div>
+        
       </WidgetBody>
     </Widget>
   );
